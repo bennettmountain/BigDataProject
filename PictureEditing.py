0aa457d018433c8bf9ecd27c2cb68a3dc399bdf8
arjun-natarajan99 <arjun.nat18@gmail.com>
2019-03-25 19:17:02 -0700

    Data Generation

--- PictureEditing.py
+++ PictureEditing.py
@@ -2,10 +2,11 @@ from PIL import Image, ImageDraw
 
 highHeel = Image.open('high_heel.jpg')
 
+
 width, height = highHeel.size
 
 
-endPoints = [(395, 210), (250, 395)]
+endPoints = [(395, 210), (250, 340)]
 
 draw = ImageDraw.Draw(highHeel)
 draw.line(endPoints, fill='red')
@@ -24,18 +25,31 @@ def drawLines(points, numLines):
 	draw = ImageDraw.Draw(highHeel)
 	#rightX and bottomY coordinates stay constant
 	rightPtX, rightPtY = points[0][0], points[0][1]
-	botPtX, botPtY = points[1][0], points[1][1]
+	leftPtX, leftPtY = points[1][0], points[1][1]
 
 	for i in list(range(1, numLines+1)):
 		# shift the point on the bottom of the image left
 		# shift the point on the side of the image down
-		rightPtY+= i*10
-		botPtX+= i*10
+		rightPtY += 3
+	#	leftPtX += 10
 
-		ends = [(rightPtX, rightPtY), (botPtX, botPtY)]
+		ends = [(rightPtX, rightPtY), (leftPtX, leftPtY)]
 		draw.line(ends, fill='red')
-	highHeel.save('try2.png')
-
+		tryi_string = 'try' + str(i) + '.png'
+		tryi = Image.open('high_heel.jpg')
+		draw = ImageDraw.Draw(tryi)
+		tiwidth,tiheight = tryi.size
+		highHeel.save(tryi_string)
+		for j in range(tiwidth) :
+			for k in range(tiheight):
+				point = [(j,k)]
+				distance = (j - leftPtX)*(rightPtY - leftPtY) - (k - leftPtY)*(rightPtX - leftPtX)
+				if distance < 0:
+					draw.point(point,fill='white')
+		tryi.save(tryi_string)
 	firsty = Image.open('try2.png')
 	firsty.show()
+drawLines(endPoints,100)
+
+
 
