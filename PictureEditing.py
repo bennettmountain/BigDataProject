9ac975d68a75bee49c07139e65bcc409b3525c71
arjun-natarajan99 <arjun.nat18@gmail.com>
2019-05-05 22:12:43 -0700

    Ahhhh

--- PictureEditing.py
+++ PictureEditing.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageDraw
 
 def drawLines(points, numLines):
 	global fileIndex
+	
 	"""
 	shifts the two endpoints and draws numLines lines on the image, 
 	visualizing possible breaks
@@ -33,6 +34,63 @@ def drawLines(points, numLines):
 					draw.point(point,fill='white')
 		tryi.save(tryi_string)
 		fileIndex += 1
+
+def drawStrongLines(numIters, starting, length, width):
+	global file2Index
+	
+	
+	#rightX and bottomY coordinates stay constant
+	# 359-380 x, 280 y
+	
+	leftPtY = starting[1] - 1
+	
+	for i in list(range(1, numIters + 1)):
+		leftPtX = int(starting[0] - 40/i) 
+		endPtX = leftPtX + 10 
+		tryi = highHeel.copy()
+		tiwidth,tiheight = tryi.size
+		draw = ImageDraw.Draw(tryi)
+		leftPtY -= 1
+		tryi_string = 'Newtry/' + 'try' + str(file2Index) + '.png'
+		
+		firstpoints = [(leftPtX, leftPtY), (endPtX, leftPtY)]
+		draw.line(firstpoints,fill = 'red')
+		for k in list(range(leftPtY, tiheight)):
+			for j in list(range(leftPtX,endPtX)):
+				point = [(j,k)]
+				#draw.point(point,fill='white')
+
+		verticalendPtY = leftPtY + length
+		firstVertPoints = [(endPtX,leftPtY), (endPtX,verticalendPtY)]
+		draw.line(firstVertPoints, fill = 'red')
+		
+		wide = int(width + (4.5*i/numTries) + ((8*i/numTries)/numTries)) 
+		horEndPtX = endPtX + wide
+		horPoints = [(endPtX,verticalendPtY), (horEndPtX, verticalendPtY)]
+		draw.line(horPoints, fill = 'red')
+		for k in list(range(verticalendPtY, tiheight)):
+			for j in list(range(endPtX,horEndPtX)):
+				point = [(j,k)]
+				#draw.point(point,fill='white')
+
+		vertical2endPtY = verticalendPtY - length
+		secondVertPoints = [(horEndPtX, verticalendPtY),(horEndPtX,vertical2endPtY)]
+		draw.line(secondVertPoints, fill = 'red')
+
+		finalPoints = [(horEndPtX,vertical2endPtY), (tiwidth, vertical2endPtY)]
+		draw.line(finalPoints, fill = 'red')
+		for k in list(range(vertical2endPtY, tiheight)):
+			for j in list(range(horEndPtX,tiwidth)):
+				point = [(j,k)]
+				#draw.point(point,fill='white')
+
+
+		tryi.save(tryi_string)
+		file2Index += 1
+
+
+
+
 def createDiffs(numTries):
 	for i in range(numTries):
 		
@@ -57,16 +115,17 @@ def createDiffs(numTries):
 		diffImage.save(diff_string)
 
 fileIndex = 1
+file2Index = 1
 highHeel = Image.open('high_heel.jpg')
 width, height = highHeel.size
 endPoints = [(395, 210), (250, 350)]
-numTries = 300
+starting = [353, 350]
+numTries = 170
 numIters = 10
-'''for i in range(numIters):
-	newEndPointXLeft = endPoints[0][0] + 1
-	newEndPointXRight = endPoints[1][0] + 1
-	newEndPoints = [(newEndPointXLeft, endPoints[0][1]), (newEndPointXRight,endPoints[1][1])]
-	drawLines(newEndPoints,numTries)
-'''
-createDiffs(numTries * numIters)
+drawStrongLines(numTries, starting, 6, 5)
+drawStrongLines(numTries, starting, 8, 4)
+drawStrongLines(numTries, starting, 10, 4)
+drawStrongLines(numTries, starting, 12, 2)
+drawStrongLines(numTries, starting, 4, 4)
+#createDiffs(numTries * numIters)
 
